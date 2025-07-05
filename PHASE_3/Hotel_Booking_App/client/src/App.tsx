4ca3069278f45f9ccfa35653999e34e748557e4c
dhanjiRajput <kidechadhanji@gmail.com>
2025-07-05 13:57:50 +0530

    updated

--- PHASE_3/Hotel_Booking_App/client/src/App.tsx
+++ PHASE_3/Hotel_Booking_App/client/src/App.tsx
@@ -3,16 +3,22 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
 
 const App = () => {
   const isOwnerPath=useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar/>}
+      <HotelReg/>
       <div className='min-h-[70vh]'>
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/rooms' element={<AllRooms/>}/>
+            <Route path='/rooms/:id' element={<RoomDetails/>}/>
+            <Route path='/my-bookings' element={<MyBookings/>}/>
           </Routes>
       </div>
       <Footer/>
